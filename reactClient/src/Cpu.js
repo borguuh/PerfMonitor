def2491d2dd06283ee4bbf7d5ebbf460c3f04f65
Borguuh <abdrahmanadewale@gmail.com>
2021-10-19 17:51:00 +0100

    setup widget state

---
+++ reactClient/src/Cpu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import moment from 'moment';
+
+function Cpu(props){
+    return (
+        <div>
+          <h3>CPU</h3>
+        </div>
+    );
+}
+
+export default(Info);
