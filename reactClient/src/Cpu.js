17ab7d1850428c4df5467695d11efa711c1ec580
Borguuh <abdrahmanadewale@gmail.com>
2021-10-19 18:04:10 +0100

    added widget styling and bootstrap

--- reactClient/src/Cpu.js
+++ reactClient/src/Cpu.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import moment from 'moment';
+import drawCircle from './utilities/canvasLoadAnimation';
 
 function Cpu(props){
-    return (
-        <div>
-          <h3>CPU</h3>
+    const canvas = document.querySelector(`.${props.cpuData.cpuWidgetId}`);
+    drawCircle(canvas,props.cpuData.cpuLoad);
+    console.log(props);
+    return(
+       <div className="col-sm-3 cpu">
+        <h3>CPU load</h3>
+        <div className="canvas-wrapper">
+            <canvas className={props.cpuData.cpuWidgetId} width="200" height="200"></canvas>
+            <div className="cpu-text">{props.cpuData.cpuLoad}%</div>
         </div>
-    );
+       </div>
+    )
 }
 
-export default(Info);
+export default Cpu;
